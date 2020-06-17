b7b4bd2b69081f5fe8e4a81239957bfcbc781286
Henrique Rezende <henriquerezendec@gmail.com>
2020-06-17 11:40:45 -0300

    Updates

---
+++ precache-manifest.de5f186094954e108738a14aaf0d8e55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112d79ba82ad436c289c26f5972d5844",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "8ad5c2ddec7cad82aea8",
+    "url": "/trivia-game/static/css/main.d21a144e.chunk.css"
+  },
+  {
+    "revision": "f05925efbae6b7e505b2",
+    "url": "/trivia-game/static/js/2.880ab3d8.chunk.js"
+  },
+  {
+    "revision": "27a5b62e99ddda7d59db8dc5a55349f2",
+    "url": "/trivia-game/static/js/2.880ab3d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ad5c2ddec7cad82aea8",
+    "url": "/trivia-game/static/js/main.a1c35009.chunk.js"
+  },
+  {
+    "revision": "436d1fd652f91b520a17",
+    "url": "/trivia-game/static/js/runtime-main.9783c1d7.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-game/static/media/trivia.466d153e.png"
+  }
+]);
